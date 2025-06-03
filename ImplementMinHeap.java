7d6901619ecce8654080f35f88950d25aadbc1a2
Leo Irakliotis <lgreco@gmail.com>
2025-06-03 20:26:34 +0000

    minheap class

--- ImplementMinHeap.java
+++
@@ -1,6 +0,0 @@
-public class ImplementMinHeap {
-        public static void main(String[] args) {
-        MinHeap demo = new MinHeap(10);
-        System.out.println(demo.getSize());
-    }
-}
---
+++ MinHeap_Implement.java
@@ -0,0 +1,9 @@
+/**
+ * Implementation class for MinHeap
+ */
+public class MinHeap_Implement {
+    public static void main(String[] args) {
+        MinHeap demo = new MinHeap(10);
+        System.out.println(demo.getSize());
+    }
+} // class MinHeap_Implement
