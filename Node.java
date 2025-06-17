d62f57a9afe3c7e38051401b43a251c75a271ef2
Leo Irakliotis <lgreco@gmail.com>
2025-06-17 02:42:12 +0000

    Node class

--- Node.java
+++ Node.java
@@ -1,21 +1,60 @@
+/**
+ * Standard node class for single and double-linked lists. The node here is
+ * written for String contents. Eventually it should be generalized for any kind
+ * of contents.
+ */
 public class Node {
 
+    /** Default value for new nodes */
+    private static final String DEFAULT_VALUE = "EDON";
+    /** Contents of node */
     private String value;
+    /** Pointer to next node */
     private Node next;
+    /** Pointer to previous node */
     private Node previous;
 
+    /** Basic constructor assigns a value to the node and two null pointers */
     public Node(String value) {
         this.value = value;
         this.next = null;
         this.previous = null;
-    }
+    } // Basic constructor
 
-    //public String toString() {
-    //    return this.value;
-    // }
+    /** Default constructor */
+    public Node() {
+        this(DEFAULT_VALUE);
+    } // default constructor
 
+    /** Accessor for value */
+    public String getValue() {
+        return value;
+    } // method getValue
+
+    /** Mutator for next */
+    public void setNext(Node next) {
+        this.next = next;
+    } // method setNext
+
+    /** Mutator for previous */
+    public void setPrevious(Node previous) {
+        this.previous = previous;
+    } // method setPrevious
+
+    /**
+     * Overrides the default Object.toString() method. Variables next and previous
+     * below will resolve thanks to their instances of Node.toString() method.
+     */
+    @Override
+    public String toString() {
+        return "Node [value=" + value + "]";
+    } // method toString
+
+    /** Simple testing */
     public static void main(String[] args) {
-        Node newNode = new Node("our first real node");
-        System.out.println(newNode);
+        Node a = new Node("our first real node");
+        Node b = new Node("our second real node");
+        a.setNext(b);
+        System.out.println(a);
     }
-}
+} // class Node
