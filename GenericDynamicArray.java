a9d4ded2f92a777604bc7190e0b3b97b79098c26
Leo Irakliotis <lgreco@gmail.com>
2025-06-24 15:46:41 +0000

    dyn arr

--- GenericDynamicArray.java
+++ GenericDynamicArray.java
@@ -2,52 +2,47 @@
  * Object adjusts the size of underlying array to accommodate any number of new
  * elements added to it. The class is written as generic.
  */
-public class DynamicArray {
+public class GenericDynamicArray<E> {
 
     private static final int DEFAULT_SIZE = 4;
-    private static final int RESIZE_FACTOR = 2;
 
     /** The underlying array of the data structure */
-    private String[] underlying;
+    private E[] underlying;
     /** How many elements have been added to the underlying array */
     private int occupancy;
 
+    @SuppressWarnings("unchecked")
     /** Basic constructor */
-    public DynamicArray(int size) {
+    public GenericDynamicArray(int size) {
         // Guard statement here to ensure that the user-provided size is legit.
         if (size < 1) {
             size = DEFAULT_SIZE;
         }
         // Create an Object array of the requested size, then cast it
         // as an array of objects E
-        this.underlying = new String[size];
+        this.underlying = (E[]) (new Object[size]);
         // At the beginning the underlying array is empty
         this.occupancy = 0;
     } // basic constructor
 
     /** Default constructor */
-    public DynamicArray() {
+    public GenericDynamicArray() {
         this(DEFAULT_SIZE);
     } // default constructor
 
     /**
      * Resize the underlying array as needed.
-     * 
-     * @param resize_by int factor multiply the size of the underlying array
      */
+    @SuppressWarnings("unchecked")
     private void resize() {
-        // Create temporary array of DOUBLE the size of the underlying array
-        String[] temp = new String[2 * this.underlying.length];
+        // Create temporary array of desired size
+        E[] temp = (E[]) new Object[2 * this.underlying.length];
         for (int i = 0; i < this.underlying.length; i++) {
             temp[i] = this.underlying[i];
         }
         this.underlying = temp;
     } // method resize
 
-    public void add(String string) {
-        // Is there room in the underlying array?
-        
-    }
 
 
 } // class DynamicArray
